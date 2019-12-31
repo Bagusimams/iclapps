09b365c43253df784f112742fad453ea7f997a5c
nisadyo <riyaniannisa@yahoo.com>
2019-12-31 06:29:38 +0000

    app.php edited online with Bitbucket

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'api_url' => env('API_URL', 'https://dev-gateway.telkomuniversity.ac.id'),
+    'api_url' => env('API_URL', 'https://gateway.telkomuniversity.ac.id'),
     'name' => env('APP_NAME', 'Program I-CLAPPS Tel-U'),
 
     /*
